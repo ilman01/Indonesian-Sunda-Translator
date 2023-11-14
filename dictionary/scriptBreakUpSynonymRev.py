981a0312453d9cad09539548f915f29fb5e602a8
ilman01 <ilmanzidni2006@gmail.com>
2023-11-14 09:49:03 +0700

    Build dictionary

---
+++ dictionary/scriptBreakUpSynonymRev.py
@@ -0,0 +1,34 @@
+def process_line(line):
+    words = line.strip().split('|')
+    left_synonyms = [left.split(',') for left in words[0].split('|')]
+    right_synonyms = [right.split(',') for right in words[1].split('|')]
+    
+    result = []
+    for left_group in reversed(left_synonyms):
+        for right_group in reversed(right_synonyms):
+            for left in reversed(left_group):
+                for right in reversed(right_group):
+                    if left[0] == " ":
+                        left = left[1:]
+                    
+                    if right[0] == " ":
+                        right = right[1:]
+
+                    result.append(f"{left}|{right}")
+    
+    return result
+
+def process_file(input_file, output_file):
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        for line in infile:
+            results = process_line(line)
+            for result in results:
+                outfile.write(result + '\n')
+
+if __name__ == "__main__":
+    input_file = input("Enter the input file name: ")
+    output_file = input("Enter the output file name: ")
+
+    process_file(input_file, output_file)
+
+    print(f"Conversion complete. Results written to {output_file}")
