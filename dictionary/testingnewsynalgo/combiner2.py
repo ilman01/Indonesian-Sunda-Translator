f5726f6b08ba25e8c610e3144dc2634ce5b1edc8
ilman01 <ilmanzidni2006@gmail.com>
2023-11-16 09:46:26 +0700

    building dictionary for new algo

---
+++ dictionary/testingnewsynalgo/combiner2.py
@@ -0,0 +1,23 @@
+def process_dictionary(input_file, output_file):
+    unique_entries = {}
+
+    with open(input_file, 'r') as infile:
+        for line in infile:
+            parts = line.strip().split('|')
+            if len(parts) == 2:
+                key, value = parts
+                if key not in unique_entries:
+                    unique_entries[key] = set([value])
+                else:
+                    unique_entries[key].add(value)
+
+    with open(output_file, 'w') as outfile:
+        for key, values in unique_entries.items():
+            outfile.write(f"{key}|{', '.join(values)}\n")
+
+# Get user input for file paths
+input_file_path = input("Enter the input file path: ")
+output_file_path = input("Enter the output file path: ")
+
+# Process the dictionary
+process_dictionary(input_file_path, output_file_path)
